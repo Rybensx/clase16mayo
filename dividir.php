2c9bc165b7d879d547c8ba80f4a6eb4a5285d5db
Rybensx <anderson_arf@hotmail.es>
2023-05-16 20:57:20 -0600

    dividir

---
+++ dividir.php
@@ -0,0 +1,17 @@
+<?php   
+
+function dividir (int $numero1, int $numero2) : void {
+    echo $numero1 / $numero2;
+}
+try {
+    dividir (10,0);
+} catch (Exception $exception){
+    echo $exception->getMessage ();
+}
+
+//function dividir (int $   numero1, int $numero2) : float {
+//    return $numero1 / $numero2;
+//}
+
+echo "<br>";
+echo "Todo funcionó correctamente";
